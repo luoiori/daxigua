b8c44e80c8b272ad0032fdabc40cbb658b521322
iori <iori.law.cn@gmail.com>
2025-12-09 18:35:13 +0800

    Fix: Remove __require from ads.js, use window._CCSettings

--- ads.js
+++ ads.js
@@ -1,35 +1,32 @@
 function noAdGoToScene() {
+    // 直接从全局访问 GameConfig，避免使用 __require
+    var settings = window._CCSettings || {};
+    var launchScene = settings.launchScene || "db://assets/Scene/MainGameScene.fire";
 
+    console.log("noAdGoToScene: Loading scene", launchScene);
 
-    var GameConfig = __require("GameConfig");
-    console.log("IndexMainMangerMaing", GameConfig.launchScene, GameConfig.Bros, GameConfig.caS);
-
-    var launchScene = GameConfig.launchScene;
-    var Bros = GameConfig.Bros;
-    var caS = GameConfig.caS;
     cc.director.loadScene(launchScene, null,
         function() {
-
             adCompleteFlag = false;
 
-            if (Bros) {
-                // show canvas
-                var canvas = document.getElementById('GameCanvas');
+            // show canvas
+            var canvas = document.getElementById('GameCanvas');
+            if (canvas) {
                 canvas.style.visibility = '';
-                var div = document.getElementById('GameDiv');
-                if (div) {
-                    div.style.backgroundImage = '';
-                }
             }
+            var div = document.getElementById('GameDiv');
+            if (div) {
+                div.style.backgroundImage = '';
+            }
+
             cc.loader.onProgress = null;
-            console.log('Success to load scene1Main: ' + launchScene);
-
-
-
-
-
+            console.log('Success to load scene: ' + launchScene);
         }
     );
+}
 
-
+// 空的 showMyAds 函数，防止错误
+function showMyAds() {
+    console.log("showMyAds called (ads disabled)");
+    // 不显示广告
 }
